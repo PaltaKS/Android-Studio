048cddc17a4a55a54a4737cd6d774a9261ff940a
Bastian Pavez <bpavez03@gmail.com>
2024-09-05 19:39:05 -0400

    Se agreggo Inicio y Registro OK

--- app/src/main/java/com/example/miprimeraaplicacionandroid/Registrar.java
+++ app/src/main/java/com/example/miprimeraaplicacionandroid/Registrar.java
@@ -4,27 +4,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.core.view.ViewCompat;
 
-public class MainActivity extends AppCompatActivity {
 
+public class Registrar extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_registrar);
+
 
 
         // Encuentra el botón por su ID
-        Button buttonToSecond = findViewById(R.id.btnCrearCuenta);
+        Button buttonToSecond = findViewById(R.id.btnCancelar);
         // Configura un listener para manejar el clic
         buttonToSecond.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick (View view){
-            // Crear un intent para ir a SecondActivity
-            Intent intent = new Intent(MainActivity.this, Registrar.class);
-            startActivity(intent);  // Iniciar la nueva actividad
+                // Crear un intent para ir a SecondActivity
+                Intent intent = new Intent(Registrar.this, MainActivity.class);
+                startActivity(intent);  // Iniciar la nueva actividad
             }
         });
     }
